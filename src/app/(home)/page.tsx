3ba49584750a0bcab45f1e5115dcb50da934e092
willtechcod <dev.pereira2019@gmail.com>
2023-10-18 00:18:38 -0300

    feat: add third  promo banner home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,13 +23,13 @@ export default async function Home() {
   });
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner src="/banner-home1.png" alt="Até 55% de desconto esse mes" />
-      <div className="mt-8 px-5">
+      <div className=" px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -39,8 +39,20 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home3.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
         <ProductList products={keyboards} />
       </div>
     </div>
