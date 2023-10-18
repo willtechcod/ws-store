0952af97ab07717396204edc46b0d02f71ce87a0
willtechcod <dev.pereira2019@gmail.com>
2023-10-18 01:16:02 -0300

    fix: ensure product prices are wrapped in one line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -9,8 +9,8 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+    <div className="flex max-w-[175px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[175px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -37,15 +37,15 @@ const ProductItem = ({ product }: ProductItemProps) => {
         <div className="flex items-center gap-2">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
-              <p className="text-xs line-through opacity-75">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="text-sm font-semibold">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
               R$ {product.basePrice.toFixed(2)}
             </p>
           )}
