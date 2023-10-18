07d5c4ae3d40a56b8d175d9fc8bc2446dc9a5299
willtechcod <dev.pereira2019@gmail.com>
2023-10-17 21:05:58 -0300

    feat: add banner discount home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home1.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto"
+      />
+    </div>
+  );
 }
